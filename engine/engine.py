ae6cb206392587c1a2e5f0cf9f60eafbd8a99707
Victoria Robles <98292916+VictoriaRobles3@users.noreply.github.com>
2023-08-30 18:02:32 -0400

    Create engine.py

---
+++ engine/engine.py
@@ -0,0 +1,8 @@
+from abc import ABC
+
+class Engine(ABC):
+    def __init__(self, last_service_date):
+        self.last_service_date = last_service_date
+
+    def needs_service(self):
+        pass
