20ba7a3c5735b886b947971570691e35563fbcfe
Victoria Robles <98292916+VictoriaRobles3@users.noreply.github.com>
2023-08-30 17:59:00 -0400

    Update spindler_battery.py

--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -2,13 +2,13 @@ from abc import ABC
 from datetime import datetime
 from battery import Battery
 
-class NubbinBattery(Battery, ABC):
+class SpindlerBattery(Battery, ABC):
     def __init__(self, last_service_date, current_date):
         super().__init__(last_service_date)
         self.current_date = current_date
 
     def needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
         if service_threshold_date < datetime.today().date():
             return True
         else:
